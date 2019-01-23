43c019234f6482e1ea5f92eef8b9d60024d738d4
vicyu <finalsteve@gmail.com>
2019-01-23 11:26:40 +0800

    update particle to rigidbody

--- vicTool/vic_actions.py
+++ vicTool/vic_actions.py
@@ -161,6 +161,8 @@ def createCameraTarget( currobj, targetName ):
     currConstraint.track_axis = 'TRACK_NEGATIVE_Z'
     currConstraint.up_axis = 'UP_Y'
 
+#===============================================    
+
 class vic_create_camera_target(bpy.types.Operator):
     bl_idname = 'vic.vic_create_camera_target'
     bl_label = 'Create Camera Target'
@@ -248,7 +250,7 @@ class VIC_ACTION_PANEL(bpy.types.Panel):
         col.operator("vic.mirror_cube_add")
         col.operator("vic.vic_create_camera_target")
         #col.operator("vic.make_meshs_plane")
-        #col.operator("vic.particle_rigidbody")
+        col.operator("vic.particle_rigidbody")
         
         row = col.row(align=True)
         row.prop(context.scene.action_properties, 'string_select_name' )
@@ -266,6 +268,106 @@ class VIC_ACTION_PANEL(bpy.types.Panel):
         #col.operator("vic.bge_quick_motion")
 #=======================================        
 
+class ParticlesToRigidbodys(bpy.types.Operator):
+    bl_idname = 'vic.particle_rigidbody'
+    bl_label = 'Particles To Rigidbodys'
+    
+    def setting(self, emitter ):
+        self.emitter = emitter
+        eval_ob = bpy.context.depsgraph.objects.get(self.emitter.name, None)
+        self.ps = eval_ob.particle_systems[0]
+        self.ps_set = self.ps.settings
+        self.ps_set.use_rotation_instance = True
+        self.start_frame = int( self.ps_set.frame_start )
+        self.end_frame = int( self.ps_set.frame_end + ( self.ps_set.lifetime *  ( 1 + self.ps_set.lifetime_random)))
+        self.update_frame = self.end_frame - self.start_frame
+        self.mesh_clone = self.ps_set.instance_object
+        self.ms = []
+
+    def createMesh(self):
+        self.emitter.select_set( False )
+        for p in self.ps.particles:
+            loc = p.location
+            rot = p.rotation.to_euler()
+            o = self.mesh_clone.copy()
+            bpy.context.view_layer.active_layer_collection.collection.objects.link(o)
+            o.scale[0] = p.size
+            o.scale[1] = p.size
+            o.scale[2] = p.size
+            o.select_set( True )
+            bpy.context.view_layer.objects.active = o
+            bpy.ops.rigidbody.object_add()
+            o.location = loc
+            o.rotation_euler= rot
+            o.rigid_body.kinematic=True
+            o.rigid_body.restitution = .2
+            o.keyframe_insert('rigid_body.kinematic')
+            o.select_set( False )
+            self.ms.append( o )
+            
+    def moveMesh(self, forceEnd = False):
+        for i, p in enumerate( self.ps.particles ):
+            o = self.ms[i]
+            if forceEnd or (p.alive_state == 'DEAD'):
+                if o.rigid_body.kinematic:
+                    o.rigid_body.kinematic=False
+                    o.keyframe_insert('rigid_body.kinematic')
+            elif p.alive_state == 'ALIVE':
+                o.location = p.location
+                o.rotation_euler = p.rotation.to_euler()
+                o.keyframe_insert('location')
+                o.keyframe_insert('scale')
+                o.keyframe_insert('rotation_euler')
+            else:
+                o.location = p.location
+                o.rotation_euler = p.rotation.to_euler()
+                
+    def clearSetting(self):
+        self.ms = []        
+            
+    def update( self, f ):
+        #print( f )
+        #print(self.end_frame)
+        if f == self.start_frame:
+            print("create")
+            self.clearSetting()
+            self.createMesh()
+        elif f == self.end_frame - 1:
+            print("end")
+            self.moveMesh(True)
+            self.clearSetting()
+        else:
+            print("move")
+            self.moveMesh()
+        
+    def executeAll(self):
+        for i in range( self.update_frame ):
+            f = self.start_frame + i;
+            bpy.context.scene.frame_set(f)
+            self.update(f)
+            print( 'frame solved:', f )
+        '''
+        for i in range( self.end_frame ):
+            f = i+1;
+            bpy.context.scene.frame_set(f)
+            self.update(f)
+            print( 'frame solved:', f )
+        '''    
+    def execute(self, context):
+        if context.view_layer.objects.active == None:
+            self.report( {'ERROR'}, 'please pick one object!' )
+            return {'FINISHED'}
+        elif len( context.view_layer.objects.active.particle_systems ) == 0:
+            self.report( {'ERROR'}, 'need particle system!' )
+            return {'FINISHED'}
+        elif context.view_layer.objects.active.particle_systems[0].settings.instance_object == None:
+            self.report( {'ERROR'}, 'particle system duplicate object need to be setting!' )
+            return {'FINISHED'}                    
+        else:
+            self.setting(context.view_layer.objects.active)
+            self.executeAll()
+        return {'FINISHED'}
+
 classes = (
     # ui
     ActionProperties,
@@ -276,7 +378,8 @@ classes = (
     vic_select_by_name,
     mirror_cube_add,
     vic_hand_drag,
-    vic_healing_all_effect_objects
+    vic_healing_all_effect_objects,
+    ParticlesToRigidbodys
 )
 def register():
     for cls in classes: bpy.utils.register_class(cls)
