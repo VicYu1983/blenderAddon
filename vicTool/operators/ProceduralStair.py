94dc0372101c4f561f309aadc7f8c2e2e730a245
vicyu <finalsteve@gmail.com>
2019-03-29 12:41:25 +0800

    prepare for procedural stair

--- vicTool/operators/ProceduralStair.py
+++ vicTool/operators/ProceduralStair.py
@@ -1,19 +1,48 @@
 import bpy
-
+from bpy.props import (
+        BoolProperty,
+        EnumProperty,
+        FloatProperty,
+        FloatVectorProperty,
+        IntProperty,
+        IntVectorProperty,
+        StringProperty,
+        )
 class vic_procedural_stair(bpy.types.Operator):
     bl_idname = 'vic.vic_procedural_stair'
     bl_label = 'Create Stair'
     bl_description = ''
     bl_options = {'REGISTER', 'UNDO'}
+
+    width = FloatProperty(
+        name='Width',
+        description='',
+        min=1.0,
+        default=1.0
+        )
         
     def doEffect( self ):  
         print("doEffect")
     def execute(self, context):
         self.doEffect()
+
+        
+        verts = [(-self.width,-1,0), (-1,1,0),(1,1,0),(1,-1,0)]
+        faces = [(0, 1, 2, 3)]
+
+
+
+        mesh = bpy.data.meshes.new("stair_mesh")
+        obj = bpy.data.objects.new("stair_obj", mesh)
+
+        mesh.from_pydata(verts, [], faces)
+
+        bpy.context.view_layer.active_layer_collection.collection.objects.link(obj)
+
         return {'FINISHED'}
 
     def draw(self, context):
         layout = self.layout
 
         box = layout.box()
-        box.label(text="addc")
+        box.prop(self, "width")
