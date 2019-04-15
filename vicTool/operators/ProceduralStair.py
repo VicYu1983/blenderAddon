2f4eef8048075bcfcbc58292833478d57bf0ebd7
vicyu <finalsteve@gmail.com>
2019-04-15 17:16:18 +0800

    update

--- vicTool/operators/ProceduralStair.py
+++ vicTool/operators/ProceduralStair.py
@@ -1,4 +1,4 @@
-import bpy
+import bpy, time
 from math import *
 from mathutils import Vector
 from bpy.props import (
@@ -11,6 +11,61 @@ from bpy.props import (
         StringProperty,
         PointerProperty
         )
+
+def addObject( obj ):
+    bpy.context.view_layer.active_layer_collection.collection.objects.link(obj)
+
+def activeObject( obj ):
+    bpy.context.view_layer.objects.active = obj
+
+def copyObject(obj, sameData = False):
+    newobj = obj.copy()
+    if not sameData:
+        newobj.data = obj.data.copy()
+        newobj.animation_data_clear()
+    return newobj
+
+def focusObject(obj):
+    # unselect all of object, and then can join my own object
+    for obj in bpy.context.view_layer.objects:
+        obj.select_set(False)
+    obj.select_set(True)
+    activeObject(obj)
+
+class vic_procedural_stair_save(bpy.types.Operator):
+    bl_idname = 'vic.vic_procedural_stair_save'
+    bl_label = 'Collapse Mesh'
+    bl_description = ''
+
+    data = StringProperty()
+
+    def execute(self, context):
+
+        # wallName = ''
+        # try:
+        #     wallName = bpy.context.scene['cwn']
+        #     #wallName = eval(self.data)[0]
+        # except:
+        #     print("not have myname")
+        
+        # print( 'select_name:', wallName )
+        # if wallName != '':
+        #     objs = []
+        #     for b in bpy.data.objects:
+        #         find_str = b.name.find( wallName )
+        #         print( 'find_str:', find_str )
+        #         b.select_set( False )
+        #         if find_str != -1:
+        #             b.hide_viewport = False
+        #             b.select_set( True )
+        #             objs.append(b)
+        #             print("add")
+        #     print( len(objs))
+        #     if len(objs) > 0:
+        #         activeObject(objs[0])
+        #         bpy.ops.object.join()
+
+        return {'FINISHED'}
 
 class vic_procedural_stair(bpy.types.Operator):
     bl_idname = 'vic.vic_procedural_stair'
@@ -44,6 +99,7 @@ class vic_procedural_stair(bpy.types.Operator):
     count = IntProperty(
         name='Step Count',
         min=1,
+        max=50,
         default=10
     )
 
@@ -51,6 +107,17 @@ class vic_procedural_stair(bpy.types.Operator):
         name='Step Depth',
         min=.1,
         default=1.
+    )
+
+    # showWall = BoolProperty(
+    #     name='Show Wall',
+    #     default=True
+    # )
+
+    editMode = BoolProperty(
+        name='Edit Mode',
+        default=True,
+        description='Turn off and press [Create Stair] again to get result mesh!'
     )
 
     wallMesh = StringProperty(
@@ -118,9 +185,17 @@ class vic_procedural_stair(bpy.types.Operator):
             )
             self.verts.append( newpos )
 
-    def createWall(self, mesh):
-        print("createWall")
+    def assignMaterial(self, obj):
+        # create material
+        if not 'StairMaterial' in bpy.data.materials:
+            bpy.data.materials.new(name="StairMaterial")
+        mat = bpy.data.materials.get("StairMaterial")
+        if obj.data.materials:
+            obj.data.materials[0] = mat
+        else:
+            obj.data.materials.append(mat)
 
+    def createWall(self, mesh):
         stepHeight = self.height / self.count
         totalLength = self.count * self.stepDepth
         startPos = Vector((self.stepDepth / 2, 0, stepHeight))
@@ -135,14 +210,68 @@ class vic_procedural_stair(bpy.types.Operator):
         radian = Vector((1,0,0)).angle(connect.normalized())
         tanRadian = tan(radian)
 
+        #create mesh in the same object
+        # for i in range(count):
+        #     wallPos = wallSingle * (i + .5)
+        #     self.createOneWall(mesh, scaleFactor, tanRadian, startPos, wallPos, self.width/2-self.wallOffsetY)
+        #     self.createOneWall(mesh, scaleFactor, tanRadian, startPos, wallPos, -self.width/2+self.wallOffsetY)
+
+        # create mesh for every wall
+        wallPrototype = copyObject(mesh)
+        for v in wallPrototype.data.vertices:
+            pos = v.co
+            v.co = (
+                pos.x * scaleFactor, 
+                pos.y, 
+                pos.z + tanRadian * (pos.x * scaleFactor)
+            )
+
+        wallObj = []
         for i in range(count):
             wallPos = wallSingle * (i + .5)
-            self.createOneWall(mesh, scaleFactor, tanRadian, startPos, wallPos, self.width/2-self.wallOffsetY)
-            self.createOneWall(mesh, scaleFactor, tanRadian, startPos, wallPos, -self.width/2+self.wallOffsetY)
+            cloneWall = copyObject(wallPrototype, True)
+            cloneWall.location.x = wallPos.x + startPos.x
+            cloneWall.location.y = wallPos.y + startPos.y + self.width/2-self.wallOffsetY
+            cloneWall.location.z = wallPos.z + startPos.z + self.wallHeight
+            addObject(cloneWall)
+            wallObj.append(cloneWall)
+
+            cloneWall = copyObject(wallPrototype, True)
+            cloneWall.location.x = wallPos.x + startPos.x
+            cloneWall.location.y = wallPos.y + startPos.y + -self.width/2+self.wallOffsetY
+            cloneWall.location.z = wallPos.z + startPos.z + self.wallHeight
+            addObject(cloneWall)
+            wallObj.append(cloneWall)
+
+        bpy.data.objects.remove(wallPrototype, do_unlink=True)
+
+        for obj in bpy.context.view_layer.objects:
+            obj.select_set(False)
+        for wall in wallObj:
+            wall.select_set(True)
+        self.startObject.select_set(True)
+        activeObject(self.startObject)
+
+        # 因爲不能動態join，所以只能利用打開這個功能時的預設值來決定要不要join
+        if not self.editMode:
+            bpy.ops.object.join()
+
+        # 好像沒辦法動態join
+        # for obj in bpy.context.view_layer.objects:
+        #     obj.select_set(False)
+        # for wall in wallObj:
+        #     wall.select_set(True)
+        # self.startObject.select_set(True)
+        # activeObject(self.startObject)
+        # bpy.ops.object.join()
 
     def execute(self, context):
         self.verts = []
         self.faces = []
+
+        # unselect all of object, and then can join my own object
+        for obj in bpy.context.view_layer.objects:
+            obj.select_set(False)
 
         x = self.width / 2
         stepHeight = self.height / self.count
@@ -164,18 +293,26 @@ class vic_procedural_stair(bpy.types.Operator):
                 (i*stepDepth,x,i*stepHeight+stepHeight)
                 ], (stepDepth, 0, 0), self.verts, self.faces, flip=True)
         
+        mesh = bpy.data.meshes.new("Stair")
+        obj = bpy.data.objects.new("Stair", mesh)
+        self.startObject = obj
+        mesh.from_pydata(self.verts, [], self.faces)
+        addObject(obj)
+
+        self.assignMaterial(obj)
+            
+        # check the name of object in the scene! if not, set value to empty
+        try:
+            bpy.context.view_layer.objects[self.wallMesh]
+        except:
+            self.wallMesh = ''
         meshName = self.wallMesh
         if (meshName != ''):
-            wallMesh = bpy.data.objects[meshName]
+            wallMesh = bpy.context.view_layer.objects[meshName]
             if wallMesh.type == 'MESH':
                 self.createWall(wallMesh)
             else:
                 print('Please Select Mesh Object!')
-
-        mesh = bpy.data.meshes.new("stair_mesh")
-        obj = bpy.data.objects.new("stair_obj", mesh)
-        mesh.from_pydata(self.verts, [], self.faces)
-        bpy.context.view_layer.active_layer_collection.collection.objects.link(obj)
 
         return {'FINISHED'}
 
@@ -197,7 +334,13 @@ class vic_procedural_stair(bpy.types.Operator):
         row = box.row()
         row.prop(self, 'wallHeight')
         row.prop(self, 'wallOffsetY')
-        box.prop_search(self, "wallMesh", scene, "objects")
+
+        row = box.row()
+        row.prop(self, 'editMode')
+        row.prop_search(self, "wallMesh", scene, "objects")
+        #box.prop(self, 'editMode')
+        #row.operator('vic.vic_procedural_stair')
+        #row.operator('vic.vic_procedural_stair_save').data = repr([self.wallMesh])
 
         box = layout.box()
         box.label(text='Pile')
