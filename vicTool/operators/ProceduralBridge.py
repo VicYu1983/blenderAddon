b0c647475689839c491f4033ed7f6b1f109bb546
vicyu <finalsteve@gmail.com>
2019-04-24 16:30:24 +0800

    update

--- vicTool/operators/ProceduralBridge.py
+++ vicTool/operators/ProceduralBridge.py
@@ -45,8 +45,12 @@ class vic_procedural_bridge(bpy.types.Operator):
 
     def execute(self, context):
         # parameters
+
+        if self.baseMesh not in bpy.context.view_layer.objects:
+            return {'FINISHED'}
+
         prefab_step = bpy.data.objects['Cube']
-        prefab_base = bpy.data.objects['Plane']
+        prefab_base = bpy.context.view_layer.objects[self.baseMesh]
         allLength = prefab_base.dimensions.x + .5
         height = 10
         powFactor = 2
@@ -57,15 +61,18 @@ class vic_procedural_bridge(bpy.types.Operator):
         useLength = allLength / count
         xDir = Vector((1,0,0))
 
-        # =======================
+        # 正常來説應該用這裏的方式。但是他在特定情況下不會更新matrix_world，所以改爲以下的方式
+        # base = copyToScene(prefab_base)
+        # base.location.x = allLength/2
+        # base.location.y = 0
+        # base.location.z = 0
+        # bpy.context.scene.update()
 
+        # 强制修改matrix_world
         base = copyToScene(prefab_base)
-        base.location.x = allLength/2
-        base.location.y = 0
-        base.location.z = 0
-
-        # update object matrix
-        bpy.context.scene.update()
+        base.matrix_world.row[0][3] = allLength/2
+        base.matrix_world.row[1][3] = 0
+        base.matrix_world.row[2][3] = 0
 
         pts = []
         for i in range( count + 1 ):    
@@ -94,5 +101,7 @@ class vic_procedural_bridge(bpy.types.Operator):
             stepObj.scale.x = scaleX
             
             self.transformBridge(base, first, second )
+        #for v in base.data.vertices:
+        #    print( v.co )
             
         return {'FINISHED'}
